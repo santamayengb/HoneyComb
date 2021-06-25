449ec82d65361a7318578e5aaf720dc155cee528
santamayengb <19mca8232@cuchd.in>
2021-06-25 18:59:02 +0530

    Scaffold();

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,21 @@ void main() {
 class HoneyComb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Loginpage(),
+    );
+  }
+}
+
+class Loginpage extends StatefulWidget {
+  @override
+  _LoginpageState createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
