e3d2275e710ed6ad83876ae9c808d2b2fdf51dde
santamayengb <19mca8232@cuchd.in>
2021-06-26 07:41:11 +0530

    upto navbar

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,46 @@ class Loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                      Text(
+                        "HoneyComb",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                          icon: Icon(Icons.notifications), onPressed: () {}),
+                      IconButton(
+                          icon: Icon(Icons.usb_rounded), onPressed: () {})
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                    color: Colors.black12,
+                    child: Text("Contaier"),
+                  ))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
